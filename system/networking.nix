27987942126bd3d429ab6f59d71cb602640c375b
Artur Borysov <arthur.borisow@gmail.com>
2025-11-14 23:03:39 +0200

    New cpu

--- system/networking.nix
+++ system/networking.nix
@@ -1,11 +1,12 @@
-{ ... }:
+{ lib, ... }:
 
 {
   networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  
+  networking.useDHCP = lib.mkDefault true;
+
   users.users.artur = {
     extraGroups = [ "networkmanager" ];
   };
